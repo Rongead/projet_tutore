213a2f2c8d4607895168d58388f7909e07c35dca
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-01-07 15:19:07 +0100

    début scan java

--- dossier_travail/java/temp.java
+++ dossier_travail/java/temp.java
@@ -4,11 +4,38 @@
  */
 
 import iut.algo.*;
+import java.io.*;
+import java.util.*;
 
 public class
 {
      public static void main(String[] args)
      {
-       System.out.println("sauce");
+          String source, racine;
+
+          System.out.print("Entrez la source du fichier html : ");
+          source = Clavier.lireString();
+          System.out.print("Entrez la répertoire de destination : ");
+          racine = Clavier.lireString();
+
+          try
+          {
+               Scanner sc = new Scanner( new FileReader( source ) );
+               PrintWriter pw = new PrintWriter ( new FileWriter (racine + "/sortie.html") );
+               pw.write("<!DOCTYPE html>");
+               pw.write(<html>);
+               pw.write(     <head>);
+               pw.write(     </head>);
+               pw.write(     <body>);
+               while( sc.hasNext() )
+               {
+
+               }
+               sc.close();
+          }
+          catch ( Exception e )
+          {
+               e.printStackTrace();
+          }
      }
 }
