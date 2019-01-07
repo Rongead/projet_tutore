de23be298301d02a2ec1016fd9176a175122ba4c
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-01-07 16:20:34 +0100

    s

--- dossier_travail/java/temp.java
+++ dossier_travail/java/temp.java
@@ -9,36 +9,36 @@ import java.util.*;
 
 public class temp
 {
-public static void main(String[] args)
-{
-     String source, racine;
-
-     System.out.print("Entrez la source du fichier html : ");
-     source = Clavier.lireString();
-     System.out.print("Entrez le répertoire de destination : ");
-     racine = Clavier.lireString();
-
-     try
+     public static void main(String[] args)
      {
-          Scanner sc = new Scanner( new FileReader( source ) );
-          PrintWriter pw = new PrintWriter ( new FileWriter (racine + "/sortie.html") );
-          pw.write("<!DOCTYPE html>");
-          pw.write("<html>");
-          pw.write("     <head>");
-          pw.write("          <title></title>")
-          pw.write("          <meta charset=UTF-8>")
-          pw.write("          <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
-          pw.write("     </head>");
-          pw.write("     <body>");
-          while( sc.hasNext() )
+          String source, racine;
+
+          System.out.print("Entrez la source du fichier html : ");
+          source = Clavier.lireString();
+          System.out.print("Entrez le répertoire de destination : ");
+          racine = Clavier.lireString();
+
+          try
           {
-               ligne = sc.nextLine();
+               Scanner sc = new Scanner( new FileReader( source ) );
+               PrintWriter pw = new PrintWriter ( new FileWriter (racine + "/sortie.html") );
+               pw.write("<!DOCTYPE html>");
+               pw.write("<html>");
+               pw.write("     <head>");
+               pw.write("          <title></title>")
+               pw.write("          <meta charset=UTF-8>")
+               pw.write("          <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
+               pw.write("     </head>");
+               pw.write("     <body>");
+               while( sc.hasNext() )
+               {
+                    ligne = sc.nextLine();
+               }
+               sc.close();
           }
-          sc.close();
+          catch ( Exception e )
+          {
+               e.printStackTrace();
+          }
      }
-     catch ( Exception e )
-     {
-          e.printStackTrace();
-     }
-}
 }
