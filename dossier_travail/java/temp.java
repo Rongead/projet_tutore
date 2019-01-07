fdd7d706597fff5aa68adef808b4da03c5c39000
Aubinneveu <aubicinq@gmail.com>
2019-01-07 16:15:47 +0100

    aubin

--- dossier_travail/java/temp.java
+++ dossier_travail/java/temp.java
@@ -9,37 +9,36 @@ import java.util.*;
 
 public class
 {
-     public static void main(String[] args)
-     {
-          String source, racine;
+    public static void main(String[] args)
+    {
+        String source, racine;
 
-          System.out.print("Entrez la source du fichier html : ");
-          source = Clavier.lireString();
-          System.out.print("Entrez le répertoire de destination : ");
-          racine = Clavier.lireString();
+        System.out.print("Entrez la source du fichier html : ");
+        source = Clavier.lireString();
+        System.out.print("Entrez le répertoire de destination : ");
+        racine = Clavier.lireString();
 
-          try
-          {
-               Scanner sc = new Scanner( new FileReader( source ) );
-               PrintWriter pw = new PrintWriter ( new FileWriter (racine + "/sortie.html") );
-               pw.write("<!DOCTYPE html>");
-               pw.write("<html>");
-               pw.write("     <head>");
-               pw.write("          <title></title>")
-               pw.write("          <meta charset=UTF-8")
-               pw.write("          <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
-               pw.write("     </head>");
-               pw.write("     <body>");
-               while( sc.hasNext() )
-               {
-                    ligne = sc.nextLine();
-
-               }
-               sc.close();
-          }
-          catch ( Exception e )
-          {
-               e.printStackTrace();
-          }
-     }
+        try
+        {
+            Scanner sc = new Scanner( new FileReader( source ) );
+            PrintWriter pw = new PrintWriter ( new FileWriter (racine + "/sortie.html") );
+            pw.write("<!DOCTYPE html>");
+            pw.write("<html>");
+            pw.write("     <head>");
+            pw.write("          <title></title>")
+            pw.write("          <meta charset=UTF-8")
+            pw.write("          <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
+            pw.write("     </head>");
+            pw.write("     <body>");
+            while( sc.hasNext() )
+            {
+                ligne = sc.nextLine();
+            }
+            sc.close();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
 }
