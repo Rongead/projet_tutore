f703dea1d047575a0b5cc037e3529d9c6ddec5c3
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-01-08 16:34:57 +0100

    ton programme il pu la merde

--- dossier_travail/java/patriceVersion.java
+++ dossier_travail/java/patriceVersion.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 
 
 
-public class Exemple2
+public class patriceVersion
 {
 	public static void main(String[] a)
 	{
@@ -24,15 +24,15 @@ public class Exemple2
 			scIn = new Scanner ( new FileInputStream ( "exemple.data"), "utf-8"  );
 			pw = new PrintWriter ( new OutputStreamWriter ( new FileOutputStream("sortie2.html"), "utf-8" ) );
 
-			pw.write("<!DOCTYPE html>\n"                                                               +
-                     "<html>\n"                                                                        +
-    				"     <head>\n"                                                                    +
-            		"          <title></title>\n"                                                      +
-            		"          <meta charset=\"UTF-8\">\n"                                             +
-            		"          <link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">\n"   +
-					"          <link rel=\"icon\" type=\"image/png\" href=\"images/maxi_logo.png\">\n" +
-            		"     </head>\n"                                                                   +
-            		"     <body>\n"                                                                     );
+			pw.write("<!DOCTYPE html>\n"                                                                +
+			         "<html>\n"                                                                         +
+			         "     <head>\n"                                                                    +
+			         "          <title></title>\n"                                                      +
+			         "          <meta charset=\"UTF-8\">\n"                                             +
+			         "          <link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">\n"   +
+			         "          <link rel=\"icon\" type=\"image/png\" href=\"images/maxi_logo.png\">\n" +
+			         "     </head>\n"                                                                   +
+			         "     <body>\n"                                                                     );
 
 			while ( scIn.hasNextLine() )
 			{
@@ -40,92 +40,101 @@ public class Exemple2
 
 				if (!(ligne.substring(0).equals(""))) {
 					switch (ligne.substring(0,3)){
-					case "TP:": pw.println ("\t<header><img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n" +
-											ligne.substring(3)                                                         +
-											"\n<img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n</header>"   ); cptPS = cptPC = 0;break;
-					case "T1:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h1>"+ ligne.substring(3)+"</h1>"); cptPS = cptPC = 0;break;
-					case "T2:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h2>"+ ligne.substring(3)+"</h2>"); cptPS = cptPC = 0;break;
-					case "t2:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h3>"+ ligne.substring(3)+"</h3>"); cptPS = cptPC = 0;break;
-					case "DP:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}if(cptDP==0){pw.println ("\t<article>");}else{pw.println ("\t</article>\n\t<article>");}cptDP++; cptPS = cptPC = 0; break;
-
-					case "PS:": if (cptPC != 0) {cptPC=0;pw.println("</p>");}if(cptPS==0){pw.println ("\t<p>"+  ligne.substring(3));}else{pw.println (ligne.substring(3));}cptPS++; break;
-					case "PC:": if (cptPS != 0) {cptPS=0;pw.println("</p>");}if(cptPC==0){pw.println ("\t<p class=\"encadrer\">"+  ligne.substring(3));}else{pw.println (ligne.substring(3));}cptPC++; break;
-				}
+						case "TP:":
+							fermetureBalisesP(pw, cptPC, cptPS);
+							pw.write ("\t<header><img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n" + ligne.substring(3) + "\n<img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n</header>\n"   );
+							cptPS = cptPC = 0;
+							break;
+						case "T1:":
+							if (cptPC != 0 || cptPS != 0)
+							{
+								cptPS=0;cptPC=0;pw.write("</p>\n");
+							}
+							pw.write ("\t\t<h1>"+ ligne.substring(3)+"</h1>\n");
+							cptPS = cptPC = 0;
+							break;
+						case "T2:":
+							fermetureBalisesP(pw, cptPC, cptPS);
+							pw.write ("\t\t<h2>"+ ligne.substring(3)+"</h2>\n");
+							cptPS = cptPC = 0;
+							break;
+						case "t2:":
+							fermetureBalisesP(pw, cptPC, cptPS);
+							pw.write ("\t\t<h3>"+ ligne.substring(3)+"</h3>\n");
+							cptPS = cptPC = 0;
+							break;
+						case "DP:":
+							fermetureBalisesP(pw, cptPC, cptPS);
+							if(cptDP==0)
+							{
+								pw.write ("\t<article>\n");
+							}
+							else
+							{
+								pw.write ("\t</article>\n\t<article>\n");
+							}
+							cptDP++;
+							cptPS = cptPC = 0;
+							break;
+						case "PS:":
+							if (cptPC != 0)
+							{
+								cptPC=0;
+								pw.write("\t\t\t</p>\n");
+							}
+							if(cptPS==0)
+							{
+								pw.write ("\t\t\t<p>"+  ligne.substring(3));
+							}
+							else
+							{
+								pw.write ("\t\t\t" + ligne.substring(3));
+							}
+							cptPS++;
+							break;
+						case "PC:":
+							if (cptPS != 0)
+							{
+								cptPS=0;
+								pw.write("</p>\n");
+							}
+							if(cptPC==0)
+							{
+								pw.write ("\t<p class=\"encadrer\">"+  ligne.substring(3));
+							}
+							else
+							{
+								pw.write (ligne.substring(3));
+							}
+							cptPC++;
+							break;
+					}
 
 					System.out.println (ligne.substring(3));
 				}
 			}
+			if (cptPC != 0 || cptPS != 0)
+			{
+				cptPS=0;cptPC=0;
+				pw.write("</p>\n");
+			}
 			pw.write("		</article>\n"+
-					 "	</body>\n"       +
-					 "</html>\n"          );
+			         "	</body>\n"        +
+			         "</html>\n"              );
 			pw.close();
 		}
-		catch(Exception e){ e.printStackTrace(); }
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-}
 
-import java.util.Scanner;
-import java.io.FileInputStream;
-
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-
-
-
-public class Exemple2
-{
-	public static void main(String[] a)
+	public static void fermetureBalisesP(PrintWriter pw, int cptPC, int cptPS)
 	{
-		String ligne;
-
-		Scanner     scIn;
-		PrintWriter pw;
-		int cptDP = 0;
-		int cptPS = 0;
-		int cptPC = 0;
-
-		try
+		if (cptPC != 0 || cptPS != 0)
 		{
-			scIn = new Scanner ( new FileInputStream ( "exemple.data"), "utf-8"  );
-			pw = new PrintWriter ( new OutputStreamWriter ( new FileOutputStream("sortie2.html"), "utf-8" ) );
-
-			pw.write("<!DOCTYPE html>\n"                                                               +
-                     "<html>\n"                                                                        +
-    				"     <head>\n"                                                                    +
-            		"          <title></title>\n"                                                      +
-            		"          <meta charset=\"UTF-8\">\n"                                             +
-            		"          <link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">\n"   +
-					"          <link rel=\"icon\" type=\"image/png\" href=\"images/maxi_logo.png\">\n" +
-            		"     </head>\n"                                                                   +
-            		"     <body>\n"                                                                     );
-
-			while ( scIn.hasNextLine() )
-			{
-				ligne = scIn.nextLine();
-
-				if (!(ligne.substring(0).equals(""))) {
-					switch (ligne.substring(0,3)){
-					case "TP:": pw.println ("\t<header><img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n" +
-											ligne.substring(3)                                                         +
-											"\n<img class=\"logo\" src=\"images/maxi_logo.png\" alt=\"logo\">\n</header>"   ); cptPS = cptPC = 0;break;
-					case "T1:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h1>"+ ligne.substring(3)+"</h1>"); cptPS = cptPC = 0;break;
-					case "T2:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h2>"+ ligne.substring(3)+"</h2>"); cptPS = cptPC = 0;break;
-					case "t2:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}pw.println ("\t\t<h3>"+ ligne.substring(3)+"</h3>"); cptPS = cptPC = 0;break;
-					case "DP:": if (cptPC != 0 || cptPS != 0) {cptPS=0;cptPC=0;pw.println("</p>");}if(cptDP==0){pw.println ("\t<article>");}else{pw.println ("\t</article>\n\t<article>");}cptDP++; cptPS = cptPC = 0; break;
-
-					case "PS:": if (cptPC != 0) {cptPC=0;pw.println("</p>");}if(cptPS==0){pw.println ("\t<p>"+  ligne.substring(3));}else{pw.println (ligne.substring(3));}cptPS++; break;
-					case "PC:": if (cptPS != 0) {cptPS=0;pw.println("</p>");}if(cptPC==0){pw.println ("\t<p class=\"encadrer\">"+  ligne.substring(3));}else{pw.println (ligne.substring(3));}cptPC++; break;
-				}
-
-					System.out.println (ligne.substring(3));
-				}
-			}
-			pw.write("		</article>\n"+
-					 "	</body>\n"       +
-					 "</html>\n"          );
-			pw.close();
+			cptPS=0;cptPC=0;
+			pw.write("</p>\n");
 		}
-		catch(Exception e){ e.printStackTrace(); }
 	}
 }
